d71762a7436d9738e4d9a9550e9efa1a773998af
deanoandrejevic <deanoandrejevic@gmail.com>
2021-05-20 17:47:28 +0000

    fourth commit

--- script.js
+++ script.js
@@ -14,13 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
+
 });
 
 // The main game "loop", called when the script is first loaded
 // and after the user's answer has been processed
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two numbers with a value of between 1 and 25
     let add1 = Math.floor(Math.random() * 25) + 1;
@@ -138,9 +149,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2
     document.getElementById("operator").textContent = "/";
 
 }
